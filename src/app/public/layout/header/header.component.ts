76367795450d15efac478c2b4d84861c36712726
kamilwolczyk <kamil.wolczyk@gmail.com>
2017-09-12 19:03:18 +0200

    Sample architecture

--- src/app/public/layout/header/header.component.ts
+++ src/app/public/layout/header/header.component.ts
@@ -7,10 +7,9 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HeaderComponent implements OnInit {
   title: string;
-  app: string;
+  currentTime: Date;
 
   ngOnInit() {
-    this.app = "Public app";
-    this.title = "Ng4 Application";
+    this.title = "Ng4 Architecture";
   }
 }
